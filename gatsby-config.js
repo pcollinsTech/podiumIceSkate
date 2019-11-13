a830ab1bc61645e4e7530fd17d20e88a36afd06f
Phil <phil@pcollins.tech>
2019-11-13 16:35:04 +0000

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,18 +12,13 @@ const config = {
       },
     },
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#32AA9F',
-    //     theme_color: '#32AA9F',
-    //     display: 'minimal-ui',
-    //     icon: 'src/assets/images/favicon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        start_url: '/',
+        icon: 'src/assets/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
 
     'gatsby-plugin-sass',
     'gatsby-transformer-sharp',
