1201b9e9da9456c10787fd8b11f9f713c5dc030c
Phil <phil@pcollins.tech>
2019-12-13 15:37:05 +0000

    Section banner back

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Img from 'gatsby-image'
 
 const Banner = ({ img, title }) => (
-  <div id="blog_banner" className="blog_major">
+  <section id="blog_banner" className="blog_major">
     <Img fluid={img} loading="lazy" style={{ position: 'static' }} />
     <div className="inner">
       <header>
         <h1>{title}</h1>
       </header>
     </div>
-  </div>
+  </section>
 )
 
 export default Banner
